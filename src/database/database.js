c87d8ac364d046835f09b4fe2d43e45126706acb
Jeffin2 <jeffinpvp.games@gmail.com>
2026-06-14 14:25:48 -0300

    mudando banco de dados

--- src/database/database.js
+++ src/database/database.js
@@ -2,7 +2,7 @@ const Database = require("better-sqlite3");
 const path = require("path");
 
 const db = new Database(
-    path.join(__dirname, "killer.db")
+    path.join(__dirname, "PVP.db")
 );
 
 db.exec(`
